b5e660de440b2c78ebc183ce22e5ca7ac912f534
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-05-29 17:57:57 -0700

    Added Clip component to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
+import Clips from './utils/Clips'
 
 const Hero = ({ heroapi: {title, subtitle,
     btntext, img, sociallinks, videos } }) => {
@@ -6,16 +8,24 @@ const Hero = ({ heroapi: {title, subtitle,
     <>
         <div className='relative h-auto w-auto flex flex-col'>
             <div 
-                className='bg-theme clip-path h-[85vh] lg:v-[75vh] md:v-[65vh] 
-                sm:v-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'
+                className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] 
+                sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'
             ></div>
             <div className='relative opacity-100 z-20 grid items-center justify-items-center nike-container'>
                 <div className='grid items-center justify-items-center mt-28 md:mt-24'>
                     <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{title}</h1>
                     <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{subtitle}</h1>
                     <button type='button' className='button-theme bg-slate-200 shadow-slate-200 rounded-xl my-5'>{btntext}</button>
-                    {/* <div className=''>videos</div>
-                    <div className=''>sociallinks</div> */}
+                    <div className='grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto'>
+                        {videos?.map((val,i) => (
+                            <Clips 
+                                key={i}
+                                imgsrc={val.imgsrc}
+                                clip={val.clip}
+                            />
+                        ))}
+                    </div>
+                    <div className=''>sociallinks</div>
                 </div>
                 <div className=''>
                     <img 
