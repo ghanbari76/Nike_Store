07de959ff988345041ba34e0d74db000d278d2f0
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-06-01 18:46:55 -0700

    Activating the shoppingBag button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import {
   HeartIcon,
   MagnifyingGlassIcon,
   ShoppingBagIcon,
 } from "@heroicons/react/24/outline";
 import logo from "../assets/logo.png";
+import { setOpenCart } from "../app/CartSlice";
 
 const Navbar = () => {
   const [navState, setNavState] = useState(false);
+  const dispatch = useDispatch();
+
+  const onCartToggle = () => {
+    dispatch(
+      setOpenCart({
+        cartState: true,
+      })
+    );
+  };
+
   const onNavScroll = () => {
     if (window.scrollY > 30) {
       setNavState(true);
@@ -56,6 +68,7 @@ const Navbar = () => {
             </li>
             <li className="grid items-center">
               <button
+                onClick={onCartToggle}
                 type="button"
                 className="border-none outline-none active:scale-110 transition-all duration-300 relative"
               >
